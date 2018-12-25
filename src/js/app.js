4ab04427064b81562065a3b64fa289913d36aba5
jGrogin <joshuag@post.ac.il>
2018-12-25 17:34:16 +0200

    only tests left

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,31 @@
 import $ from 'jquery';
-import {parseCode} from './code-analyzer';
+import {parseCode, get_flowTree} from './code-analyzer';
+import * as js2flowchart from 'js2flowchart';
+
+function applyFillers(shapesTreeEditor) {
+    shapesTreeEditor.applyShapeStyles(
+        shape => shape, {
+            fillColor: '#ffffff'
+        });
+    shapesTreeEditor.applyShapeStyles(
+        shape => shape.getNode().inPath, {
+            fillColor: '#00ff10'
+        });
+}
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+    });
+    $('#codeFlowButton').click(() => {
+        let codeToParse = $('#codePlaceholder').val();
+        let flowTree = get_flowTree(codeToParse,JSON.parse('[' + $('#inputVector').val() + ']').reverse());
+        let shapesTree = js2flowchart.createSVGRender().buildShapesTree(flowTree);
+        const shapesTreeEditor = js2flowchart.createShapesTreeEditor(shapesTree);
+        applyFillers(shapesTreeEditor);
+        $('#parsedCode').val(shapesTree.print());
+        document.getElementById('flowChart').innerHTML = shapesTree.print();
     });
 });
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,7 +1,101 @@
 import * as esprima from 'esprima';
+import * as js2flowchart from 'js2flowchart';
+import * as staticeval from 'static-eval';
 
 const parseCode = (codeToParse) => {
     return esprima.parseScript(codeToParse);
 };
 
-export {parseCode};
+const parseCode_with_source = (codeToParse) => {
+    return esprima.parseScript(codeToParse, {loc: true, range: true}, node => node.txt =
+        codeToParse.substring(node.range[0], node.range[1]));
+};
+
+function handleBody(node, env) {
+    node.body != null ? node.body.forEach(x => handleNode(x, env)) : null;
+}
+
+function handleFunctionDeclaration(node, env) {
+    var paramList = node.name.match(/function[^(]*\(([^)]*)\)/)[1].split(/\W+/);
+    paramList.forEach(x => env.env[x] = env.inputVector.pop());
+    handleBody(node, env);
+}
+
+function isFunctionDeclaration(node) {
+    return node.type === 'Function' && node.subType === 'FunctionDeclaration';
+}
+
+function updateEnv(env, key, value) {
+    env.env[key] = value;
+    env.oldEnv != null && env.oldEnv.env[key] != null ? updateEnv(env.oldEnv, key, value) : null;
+}
+
+function updateArrayEnv(env, key, index, value) {
+    env.env[key][index] = value;
+    env.oldEnv != null && env.oldEnv.env[key] != null ? updateArrayEnv(env.oldEnv, key, value) : null;
+}
+
+function isVarDeclaration(node) {
+    return node.type === 'VariableDeclarator' || node.type === 'AssignmentExpression';
+}
+
+function handleDeclaration(node, env) {
+    let expr = node.name.startsWith('let') ? node.name.slice(3) : node.name;
+    let parsedExpr = parseCode_with_source(expr).body[0].expression;
+    let value = staticeval(parsedExpr.right, env.env);
+    parsedExpr.left.type === 'MemberExpression' ? updateArrayEnv(env, parsedExpr.left.object.txt,
+        parsedExpr.left.property.txt, value) :
+        updateEnv(env, parsedExpr.left.txt, value);
+}
+
+function isIfStatement(node) {
+    return node.type === 'Conditional' && node.subType === 'IfStatement';
+}
+
+function handleIfStatement(node, env) {
+    let parsedExpr = parseCode_with_source(node.name).body[0].expression;
+    let envCopy = JSON.parse(JSON.stringify(env));
+    envCopy.oldEnv = env;
+    staticeval(parsedExpr, env.env) ? handleNode(node.body[0], envCopy) : node.body.length > 1 ? handleNode(node.body[1], envCopy) : null;
+}
+
+function isWhileStatement(node) {
+    return node.type === 'Loop' && node.subType === 'WhileStatement';
+}
+
+function handleWhileStatement(node, env) {
+    let parsedExpr = parseCode_with_source(node.name).body[0].expression;
+    let envCopy = JSON.parse(JSON.stringify(env));
+    envCopy.oldEnv = env;
+    if (staticeval(parsedExpr, env.env)) {
+        handleBody(node, envCopy);
+        handleNode(node, env);
+    }
+}
+
+function handleNode(node, env) {
+    node.inPath = true;
+    if (isFunctionDeclaration(node)) {
+        handleFunctionDeclaration(node, env);
+    }
+    else if (isVarDeclaration(node)) {
+        handleDeclaration(node, env);
+    }
+    else if (isIfStatement(node)) {
+        handleIfStatement(node, env);
+    }
+    else if (isWhileStatement(node)) {
+        handleWhileStatement(node, env);
+    }
+    else {
+        handleBody(node, env);
+    }
+}
+
+function get_flowTree(codeToParse, inputVector) {
+    const flowTree = js2flowchart.convertCodeToFlowTree(codeToParse);
+    handleNode(flowTree, {env: {}, inputVector: inputVector});
+    return flowTree;
+}
+
+export {parseCode, get_flowTree};
